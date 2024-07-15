ce28f80744e479a36a2a9df1a726a9bd391646f2
Kan <70838508+kan6868@users.noreply.github.com>
2024-07-15 22:03:12 +0700

    update pixel perfect

--- scripts/gameplay/objects/camera.lua
+++ scripts/gameplay/objects/camera.lua
@@ -1,24 +1,33 @@
 local M = {}
 
-local min, max = math.min, math.max
+local min, max, floor = math.min, math.max, math.floor
 local cx, cy = display.contentCenterX, display.contentCenterY
 local aw, ah = display.actualContentWidth, display.actualContentHeight
+local round = function(number, idp)
+    local mult = 10 ^ (idp or 0)
+    return floor(number * mult + 0.5) / mult
+end
+
+local lerp = function(from, to, time)
+    return from + (to - from) * time
+end
+
 function M.new(instance)
     local world = instance.parent.parent -- Get the map camera -> layer -> map
     local limitBounds = world:findObjectById(instance.limit)
     local focusObject = world:findObjectById(instance.focusObject)
 
     local camera = instance
-    
+
     if limitBounds then
         limitBounds.isVisible = false
-        
+
         camera.bounds = --limitBounds.contentBounds
         {
             xMin = limitBounds.x - limitBounds.contentWidth * .5,
-            yMin = limitBounds.y - limitBounds.contentHeight *.5,
+            yMin = limitBounds.y - limitBounds.contentHeight * .5,
             xMax = limitBounds.x + limitBounds.contentWidth * .5,
-            yMax = limitBounds.y + limitBounds.contentHeight *.5
+            yMax = limitBounds.y + limitBounds.contentHeight * .5
         }
     end
 
@@ -27,19 +36,19 @@ function M.new(instance)
 
     camera.offsetH = 0
     camera.offsetV = 0
-    camera.smoothingSpd = 0
+    camera.smoothingSpd = 4
 
     camera.debugMargin = {}
 
-    camera.margin_v = false
-    camera.margin_h = false
+    camera.margin_v = true
+    camera.margin_h = true
 
     function camera:setTarget(target)
         focusObject = target
     end
 
     function camera:limit()
-        local halfWidth = aw / 2 --+ self.offsetH
+        local halfWidth = aw / 2  --+ self.offsetH
         local halfHeight = ah / 2 --+ self.offsetV
 
         if self.bounds.xMin > self.x - halfWidth then
@@ -51,30 +60,30 @@ function M.new(instance)
         end
 
         if self.bounds.yMin > self.y - halfHeight then
-            self.y = self.bounds.yMin + halfHeight 
+            self.y = self.bounds.yMin + halfHeight
         end
 
         if self.bounds.yMax < self.y + halfHeight then
-            self.y = self.bounds.yMax - halfHeight 
+            self.y = self.bounds.yMax - halfHeight
         end
 
         if self.bounds.xMin >= self.x - halfWidth and
-        self.bounds.xMax <= self.x + halfWidth then
+            self.bounds.xMax <= self.x + halfWidth then
             self.x = limitBounds.x
         end
 
         if self.bounds.yMin >= self.y - halfHeight and
-        self.bounds.yMax <= self.y + halfHeight then
-            self.y = limitBounds.y  
+            self.bounds.yMax <= self.y + halfHeight then
+            self.y = limitBounds.y
         end
     end
 
     function camera:setDragVerticle(bool)
-        self.margin_v = bool        
+        self.margin_v = bool
     end
 
     function camera:setHorizontalVerticle(bool)
-        self.margin_h = bool        
+        self.margin_h = bool
     end
 
     function camera:setDragMargin(bounds)
@@ -98,9 +107,9 @@ function M.new(instance)
         end
 
         self.margin.top = camera.camHeight / 2 * self.dragMargin.top
-        self.margin.right = camera.camWidth  / 2 * self.dragMargin.right
+        self.margin.right = camera.camWidth / 2 * self.dragMargin.right
         self.margin.bottom = camera.camHeight / 2 * self.dragMargin.bottom
-        self.margin.left = camera.camWidth  / 2 * self.dragMargin.left
+        self.margin.left = camera.camWidth / 2 * self.dragMargin.left
     end
 
     function camera:drawDragMargin()
@@ -163,7 +172,7 @@ function M.new(instance)
     end
 
     function camera:moveFollowFocusObject()
-        local moveX = self.x or 0 
+        local moveX = self.x or 0
         local moveY = self.y or 0
 
         if focusObject then
@@ -171,8 +180,8 @@ function M.new(instance)
             moveY = focusObject.y or 0
         end
 
-        local smoothing = (camera.smoothingSpd == 0) and 1 or camera.smoothingSpd * ( 1 / display.fps)
-  
+        local smoothing = (camera.smoothingSpd == 0) and 1 or camera.smoothingSpd * (1 / display.fps)
+
         local smoothMoveX = moveX
         local smoothMoveY = moveY
 
@@ -189,39 +198,44 @@ function M.new(instance)
         local deltaX = smoothMoveX - self.x
         local deltaY = smoothMoveY - self.y
 
-        self.x = self.x + deltaX * smoothing
-        self.y = self.y + deltaY * smoothing
+        self.x = round(lerp(self.x, self.x + deltaX, smoothing))
+        self.y = round(lerp(self.y, self.y + deltaY, smoothing))
     end
 
-    local function update()
+    local startTime = 0
+    local function update(evt)
         if not camera then
             return false
         end
+
+        local dt = evt.time - startTime
+        startTime = evt.time
+
         camera:moveFollowFocusObject()
-        
+
         if limitBounds then
-            camera:limit()    
+            camera:limit()
         end
-        
+
         local objx, objy = world:localToContent(camera.x, camera.y)
         objx, objy = cx - objx, cy - objy
-    
-        world.x = world.x + objx + camera.offsetH * 1/60
-        world.y = world.y + objy + camera.offsetV * 1/60
-        
+
+        world.x = world.x + objx + camera.offsetH * dt
+        world.y = world.y + objy + camera.offsetV * dt
+
         camera:drawDragMargin()
     end
 
     camera:setDragMargin()
     camera:updateDragMargin()
-    
+
     function camera:finalize()
         Runtime:removeEventListener("enterFrame", update)
     end
 
     Runtime:addEventListener("enterFrame", update)
     camera:addEventListener("finalize")
-    return camera    
+    return camera
 end
 
 return M
